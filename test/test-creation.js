a1d8814a3649219199505344aa085b5bedb23c2e
Wil Linssen <wil@linssen.me>
2014-01-20 12:26:39 +0000

    Legit tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,6 +1,8 @@
 /*global describe, beforeEach, it*/
 'use strict';
 
+var assert  = require('assert');
+var fs      = require('fs');
 var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
@@ -20,18 +22,39 @@ describe('zepackage generator', function () {
     });
 
     it('creates expected files', function (done) {
-        var expected = [
-            // add files you expect to exist here.
+        var expectedFiles;
+        expectedFiles = [
             '.jshintrc',
-            '.editorconfig'
+            '.editorconfig',
+            'Gruntfile.js',
+            'src/templates/base.html',
+            'src/static/styles/_config.scss',
+            'src/static/styles/_reset.scss',
+            'src/static/styles/_utils.scss',
+            'src/static/styles/screen.scss'
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            'appName': 'my app',
+            'serverSide': 'python.flask'
         });
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
-            helpers.assertFiles(expected);
+            var expectedAppName, files, regexes;
+            expectedAppName = 'my-app';
+            files = {
+                'base.html': fs.readFileSync('src/templates/base.html', 'utf8'),
+                'package.json':  fs.readFileSync('package.json', 'utf8')
+            };
+            regexes = {
+                'base.html': /filename=\'styles\/dist\/my-app\.css\'/,
+                'package.json': /\"name\": \"my-app\"/
+            };
+
+            helpers.assertFiles(expectedFiles);
+            assert.ok(regexes['base.html'].test(files['base.html']), 'base.html template using an incorrect appName');
+            assert.ok(regexes['package.json'].test(files['package.json']), 'package.json template using an incorrect appName');
+
             done();
         });
     });
